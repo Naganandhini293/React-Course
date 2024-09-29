c980e9a828ea3349d40f0242f1f92b78828e8477
Naganandhini293 <104862030+Naganandhini293@users.noreply.github.com>
2024-09-30 01:12:58 +0530

    Ep-6: useEffect, useState, architecture, conditional render, shimmer UI

--- EP-5/src/components/Body.js
+++ EP-5/src/components/Body.js
@@ -1,27 +1,74 @@
-import React from 'react'
-import { useState } from 'react';
-import RestarauntCard from './RestarauntCard'
-import { restaurauntMockData } from '../utils/mockData';
+import React, { useEffect, useState } from "react";
+
+import RestarauntCard from "./RestarauntCard";
+
+import { SWIGGIY_API_URL } from "../utils/constants";
 
 export default function () {
-    const [restaurauntData , setRestaurauntData] = useState(restaurauntMockData)
+  const [restaurauntData, setRestaurauntData] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestrauntData, setFilteredRestrauntData] = useState([]);
+
+  useEffect(() => {
+    fetchRestrauntData();
+  }, []);
+
+  const fetchRestrauntData = async () => {
+    const response = await fetch(SWIGGIY_API_URL, {
+      method: "GET",
+    });
+
+    const json = await response.json();
+
+    //Optional chaining
+    const restaurants =
+      json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+
+    setRestaurauntData(restaurants);
+    setFilteredRestrauntData(restaurants);
+  };
   return (
     <div className="main-ctr">
-        <button onClick={()=>{
-            const filteredrestaurauntData = restaurauntData.filter(data => data.card.card.info.avgRating > 4)
-            setRestaurauntData(filteredrestaurauntData)
-        }}>Top Restaurant</button>
-        <div className='restauraunt-list-ctr'>
-            {restaurauntData.map((data) => {
-            return (
-                <RestarauntCard
-                key={data.card.card.info.id}
-                resData={data.card.card}
-                />
+      <div>
+        <button
+          onClick={() => {
+            const filteredrestaurauntData = restaurauntData.filter(
+              (data) => data?.info?.avgRating > 4
             );
-            })}
+            setRestaurauntData(filteredrestaurauntData);
+          }}
+        >
+          Top Restaurant
+        </button>
+        <div className="search-ctr">
+          <input
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            onClick={() => {
+              const temp = restaurauntData.filter((data) =>
+                data?.info?.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+              setFilteredRestrauntData(temp);
+            }}
+          >
+            Search
+          </button>
         </div>
-        
       </div>
-  )
+
+      <div className="restauraunt-list-ctr">
+        {filteredRestrauntData.map((data) => {
+          return <RestarauntCard key={data?.info.id} resData={data} />;
+        })}
+      </div>
+    </div>
+  );
 }
--- EP-5/src/utils/constants.js
+++ EP-5/src/utils/constants.js
@@ -1,2 +1,3 @@
 export const RESTAURANT_IMAGE_URL  = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 
+export const SWIGGIY_API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.66020&lng=78.15320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
